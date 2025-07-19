cb2db8aeec42ea87bff0b8b8ba840f335dd7fda9
jgvillasboas <jgvillasboas@gmail.com>
2025-07-19 16:06:12 +0100

    add

--- Tanktek4d.tab/Info.panel/script.py
+++ Tanktek4d.tab/Info.panel/script.py
@@ -12,3 +12,19 @@ from System.Windows import Window, ResourceDictionary
 from System.Windows.Controls import CheckBox, Button, TextBox, ListBoxItem
 from System import Uri
 
+PATH_SCRIPT = os.path.dirname(__file__) #get directory name
+doc = __revit__.ActiveDocument.Document #active document in Revit (revit model open)
+uidoc = __revit__.ActiveDocument #UI document, necessary to get elements, create selections, change views, making zoom in elements, etc
+app = __revit__.Application #Revit Instance, necessary to get global configurations, families information, create/open new docs, units, exportations, etc.
+
+class AboutForm(Window):
+    def __init__(self):
+        path_xaml_file = os.path.join(PATH_SCRIPT, 'MainWindow.xaml')
+        wpf.LoadComponent(self, path_xaml_file)
+
+        self.ShowDialog()
+
+    def CloseButton_Click(self, sender, e):
+        self.Close()
+
+UI = AboutForm()
